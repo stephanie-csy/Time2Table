a8b952e960a9e631e6126fab99d9661033bb4b2b
Stephanie <stephaniechenshiyao@gmail.com>
2021-05-28 18:02:45 +0800

    Testing

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyA0662SOxbjhFXKn6grgenniWR2reEzPbU",
+    authDomain: "time2table-8114c.firebaseapp.com",
+    projectId: "time2table-8114c",
+    storageBucket: "time2table-8114c.appspot.com",
+    messagingSenderId: "959920136596",
+    appId: "1:959920136596:web:a65cc566d5c845b958af81"
+  };
---
+++ src/pages/HomePage.js
@@ -0,0 +1,21 @@
+import Box from "../components/Box";
+
+function HomePage() {
+    return (
+        <>
+            <Box>
+                <h2>Getting Started</h2>
+            </Box>
+
+            <Box>
+                <h2>Activity</h2>
+            </Box>
+
+            <Box>
+                <h2>Upcoming Meet Ups</h2>
+            </Box>
+        </>
+    );
+}
+
+export default HomePage;

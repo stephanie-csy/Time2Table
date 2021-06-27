629fe596a755c901707de665057d2d1303d9402f
Stephanie <stephaniechenshiyao@gmail.com>
2021-06-27 21:03:53 +0800

    Added feature to integrate any Google calendar into your profile page

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "@ericz1803/react-google-calendar";
 import { css } from "@emotion/react";
+import Box from "../Box";
 
 const API_KEY = "AIzaSyCYz3qO7GZBAX_U6Yj2S3QnF42mq0pKj-w";
 
@@ -15,16 +16,48 @@ let styles = {
   `
 }
 
-let calendars = [
-  {calendarId: "en.singapore#holiday@group.v.calendar.google.com"}
-];
+function Cal(props) {
+  const [calID, setCalID] = useState("Loading calendar...");
+  useEffect(() => {
+    const savedCalID = window.localStorage.getItem("calendarID");
+    setCalID(savedCalID ?? "en.singapore#holiday@group.v.calendar.google.com");
+    
+    
+  }, []);
+  if(calID === "en.singapore#holiday@group.v.calendar.google.com") {
+    return (
+  
+      <Box>
+        <h4>
+          Enter your calendar ID
+        </h4>
+        <strong
+          role="button"
+          onClick={() => {
+            const newCalID = prompt("Enter your calendar ID", calID);
+            setCalID(newCalID);
+            window.localStorage.setItem("calendarId", newCalID);
+          }}
+        >
+          {"Calendar set as: " + calID || "<set a calendar>"}
+          
+        </strong>
+        
+        <Calendar apiKey={API_KEY} calendars={[{calendarId: calID}]} styles={styles} />
+      </Box>
+    );
+  }
+  else {
+    return (
+      <Calendar apiKey={API_KEY} calendars={[{calendarId: calID}]} styles={styles} />
+    );
+  }
+}
 
 class Schedule extends React.Component {
   render() {
     return (
-      <div>
-        <Calendar apiKey={API_KEY} calendars={calendars} styles={styles} />
-      </div>
+      <Cal />
     );
   }
 }

cb887f6155daa29d76b1d975511a13da2cd8c3ce
Rebecca <rebeccalaujx@gmail.com>
2021-06-28 18:59:13 +0800

    added to addfriends and addgroups page

--- src/pages/AddFriendsPage.js
+++ src/pages/AddFriendsPage.js
@@ -1,4 +1,6 @@
 import Box from "../components/Box";
+import TextField from '@material-ui/core/TextField';
+import { Button } from "@material-ui/core";
 
 function AddFriendsPage() {
     return (
@@ -8,7 +10,14 @@ function AddFriendsPage() {
                 <h6>
                     Add a friend to start time2tabling together!
                 </h6>
-                
+                <TextField id="standard-basic" label="Search for your friend" />
+                <h1> </h1>
+                <Button
+                        variant="contained"
+                        color="primary"
+                    >
+                        Add Friend
+                    </Button>
         </Box>
     );
 }
--- src/pages/CreateGroupPage.js
+++ src/pages/CreateGroupPage.js
@@ -1,4 +1,6 @@
 import Box from "../components/Box";
+import TextField from '@material-ui/core/TextField';
+import { Button } from "@material-ui/core";
 
 function CreateGroupPage() {
     return (
@@ -8,6 +10,15 @@ function CreateGroupPage() {
                 <h6>
                     Create a group with your friends to start time2tabling together!
                 </h6>
+                <TextField id="standard-basic" label="Search for your friend" />
+                <h1> </h1>
+                <Button
+                        variant="contained"
+                        color="primary"
+                    >
+                        Create Group
+                    </Button>
+        
         </Box>
     );
 }
